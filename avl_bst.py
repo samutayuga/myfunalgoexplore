eccfcd99a7583196af258d6279ab8fab877a514d
putumas <putumas.mertayasa.e@thalesdigital.io>
2022-06-06 06:37:28 +0800

    chore: add avl implementation

---
+++ avl_bst.py
@@ -0,0 +1,167 @@
+class Node:
+    def __init__(self, data, parent):
+        self.data = data
+        self.left_node = None
+        self.right_node = None
+        self.parent = parent
+        self.height = 0
+
+
+class AVLTree:
+    def __init__(self):
+        self.root = None
+
+    def remove(self, data):
+        if self.root:
+            self.remove_node(data, self.root)
+
+    def insert(self, data):
+        if self.root is None:
+            self.root = Node(data)
+        else:
+            self.insert_node(data, self.root)
+
+    def insert_node(self, data, node):
+        # we have to consider the left subtree
+        if data < node.data:
+            # we have to check if the left node is a None
+            # when the left node is  not None
+            if node.left_node:
+                # recursive
+                self.insert_node(data, node.left_node)
+            else:
+                # create a left node
+                node.left_node = Node(data, node)
+                # update height parameter
+                node.height = max(self.calc_height(node.left_node), self.calc_height(node.right_node)) + 1
+        else:
+            # we have to check if the right node is None
+            # when the right child is not None
+            if node.right_node:
+                self.insert_node(data, node.right_node)
+            else:
+                node.right_node = Node(data, node)
+                node.height = max(self.calc_height(node.left_node), self.calc_height(node.right_node)) + 1
+        # after every insertion WE HAVE TO CHECK whether the AVL properties are violated
+        self.handle_violation(node)
+
+    def remove_node(self, data, node):
+        if node is None:
+            return
+        if data < node.data:
+            self.remove_node(data, node.left_node)
+        elif data > node.data:
+            self.remove_node(data, node.right_node)
+        else:
+            # this is the node we want to remove
+            # we have found the node we want to remove
+            # there are 3 options
+            # LEAF NODE CASE
+            if node.left_node is None and node.right_node is None:
+                print("Removing a leaf node ...%d" % node.data)
+                parent = node.parent
+                if parent is not None and parent.left_node == node:
+                    parent.left_node = None
+                if parent is not None and parent.right_node == node:
+                    parent.right_node = None
+                if parent is None:
+                    self.root = None
+
+                del node
+                # after every insertion WE HAVE TO CHECK whether the AVL properties are violated
+                self.handle_violation(node)
+            # WHEN THERE iS A SINGLE CHILD
+            elif node.left_node is None and node.right_node is not None:
+                print("Removing a node with single right child")
+                parent = node.parent
+                """
+                    4
+                 3      6 
+                1  2   5  
+                         7
+                """
+
+                if parent is not None and parent.left_node == node:
+                    parent.left_node = node.right_node
+                """
+                                    4
+                                 3      5 
+                                1  2      6
+                                            7
+               """
+                if parent is not None and parent.right_node == node:
+                    parent.right_node = node.right_node
+
+                if parent is None:
+                    self.root = node.right_node
+                node.right_node.parent = parent
+
+                del node
+                # after every insertion WE HAVE TO CHECK whether the AVL properties are violated
+                self.handle_violation(node)
+            elif node.left_node is not None and node.right_node is None:
+                print("Removing a node with single left child")
+                parent = node.parent
+                """
+                supposed we delete 6
+                      4
+                    /   \
+                  3       7 
+                /  \     / \
+                1   2   6   9
+                       /   /
+                      5   8
+                Where 7 is 6's parent node, and 6 is 7's left node
+                After removing 6, the 6's left node will become
+                7's left node
+                """
+
+                if parent is not None and parent.left_node == node:
+                    parent.left_node = node.left_node
+
+                """
+                Supposed we need to delete 9
+                      4
+                    /   \
+                  3       7 
+                /  \     / \
+                1   2   6   9
+                       /   /
+                      5   8
+               Where 7 is 9's parent node, and 9 is 7's right node
+                After removing 9, the 9's left node will become
+                7's right node
+               """
+                if parent is not None and parent.right_node == node:
+                    parent.right_node = node.left_node
+
+                if parent is None:
+                    self.root = node.left_node
+                node.left_node.parent = parent
+
+                del node
+                # after every insertion WE HAVE TO CHECK whether the AVL properties are violated
+                self.handle_violation(node)
+            # WHEN THERE iS A SINGLE CHILD
+            else:
+                print("Removing node with 2 children")
+                """
+                                Supposed we need to delete 3
+                                      4
+                                    /   \
+                                  3       7 
+                                /  \     / \
+                                1   2   6   9
+                                       /   /
+                                      5   8
+                               Where 7 is 9's parent node, and 9 is 7's right node
+                                After removing 9, the 9's left node will become
+                                7's right node
+                """
+                predecessor = self.get_predecessor(node.left_node)
+                # swap the node and predecessor
+                temp = predecessor.data
+                predecessor.data = node.data
+                node.data = temp
+
+                self.remove_node(data, predecessor)
