5706dbdfb81b9d1b7448d5f8e4eb8cdcf8191d94
putumas <putumas.mertayasa.e@thalesdigital.io>
2022-06-08 21:07:05 +0800

    chore: add descending order with bubble sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -2,15 +2,19 @@ class BubbleSort:
     def __init__(self, nums):
         self.nums = nums
 
-    def sort(self):
+    def sort(self, ascending=True):
         """
         [-10,1,1000,-9]
         :return:
         """
         for i in range(len(self.nums) - 1):
             for j in range(len(self.nums) - i - 1):
-                if self.nums[j] > self.nums[j + 1]:
-                    self.swap(j, j + 1)
+                if ascending:
+                    if self.nums[j] > self.nums[j + 1]:
+                        self.swap(j, j + 1)
+                else:
+                    if self.nums[j] < self.nums[j + 1]:
+                        self.swap(j, j + 1)
 
     def swap(self, j, k):
         """
@@ -19,4 +23,3 @@ class BubbleSort:
         :return:
         """
         self.nums[j], self.nums[k] = self.nums[k], self.nums[j]
-
--- bubble_sort_test.py
+++ bubble_sort_test.py
@@ -9,6 +9,9 @@ class BubbleSortTest(unittest.TestCase):
         bs = BubbleSort(original)
         bs.sort()
         self.assertEqual(expected, bs.nums)  # add assertion here
+        expected_desc = [1000, 10, 8, 7, -10, -11]
+        bs.sort(ascending=False)
+        self.assertEqual(expected_desc, bs.nums)  # add assertion here
 
 
 if __name__ == '__main__':
